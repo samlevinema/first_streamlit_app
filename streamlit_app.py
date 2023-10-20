0f1b8b56560011ded3ba17d53f6c781028d25ab9
samlevinema <148292973+samlevinema@users.noreply.github.com>
2023-10-20 10:04:46 -0400

    Update streamlit_app.py
    fixed fruityvice section with displaying json

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,11 +7,6 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
 
-# new section for fruityvice
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
@@ -25,3 +20,10 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
+
+# new section for fruityvice
+import requests
+streamlit.header('Fruityvice Fruit Advice')
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+streamlit.text(fruityvice_response.json())
