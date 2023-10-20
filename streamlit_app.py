506f9c021f14fe7d9cb802ca5cfeb19877831eaf
samlevinema <148292973+samlevinema@users.noreply.github.com>
2023-10-20 12:46:56 -0400

    Update streamlit_app.py
    error handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,16 +25,18 @@ streamlit.dataframe(fruits_to_show)
 
 # new section for fruityvice
 streamlit.header('Fruityvice Fruit Advice')
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# put json into a pandas 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# display result pandas as a table
-streamlit.dataframe(fruityvice_normalized)
+try:
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit_choice:
+  streamlit.error('Please enter a fruit to get information')
+else:
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  # put json into a pandas 
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  # display result pandas as a table
+  streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 streamlit.stop()
 
