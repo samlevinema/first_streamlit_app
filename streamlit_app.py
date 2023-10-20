807b0756f7a63474c0b8c25bc0f03a8c92d4baba
samlevinema <148292973+samlevinema@users.noreply.github.com>
2023-10-20 12:48:33 -0400

    Update streamlit_app.py
    fixed more indents

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,14 +27,14 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
-if not fruit_choice:
-  streamlit.error('Please enter a fruit to get information')
-else:
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-  # put json into a pandas 
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  # display result pandas as a table
-  streamlit.dataframe(fruityvice_normalized)
+  if not fruit_choice:
+    streamlit.error('Please enter a fruit to get information')
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # put json into a pandas 
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # display result pandas as a table
+    streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
   streamlit.error()
 
