e7202b648bf084734ab2c9c1dfbf50c89914b812
samlevinema <148292973+samlevinema@users.noreply.github.com>
2023-10-20 10:02:23 -0400

    Update streamlit_app.py
    new section for fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,11 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
 
+# new section for fruityvice
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
@@ -19,3 +24,4 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
